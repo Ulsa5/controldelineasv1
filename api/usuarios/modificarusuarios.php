a4c832da15080978cee837cb3c019be827fd9d51
Ulsa5 <jescobarg9@miumg.edu.gt>
2021-05-15 13:38:03 -0600

    Correccion de otra ruta (modificarusuarios)

--- api/usuarios/modificarusuarios.php
+++ api/usuarios/modificarusuarios.php
@@ -60,7 +60,7 @@ if($_POST){
 <head>
     <meta charset="UTF-8">
     <title>Modificacion</title>
-    <link rel="stylesheet" href="../../forms/usuarios/css/New.css">
+    <link rel="stylesheet" href="../../forms/usuarios/css/new.css">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 </head>
 
