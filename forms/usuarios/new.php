c0336ddffbb5ec624d8f3cb15b0dc28b90df6fe9
Ulsa5 <jescobarg9@miumg.edu.gt>
2021-05-15 13:21:38 -0600

    correccion de nombres

--- forms/usuarios/new.php
+++ forms/usuarios/new.php
@@ -7,7 +7,7 @@
 
     <meta charset="UTF-8">
     <title>Registro</title>
-    <link rel="stylesheet" href="css/New.css">
+    <link rel="stylesheet" href="css/new.css">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <script type="text/javascript" src="../../api/config/javascript.js"></script>
     
