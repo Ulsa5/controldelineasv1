ac6b93e20c8dac8e900f18f501911cf2742249a3
Ulsa5 <jescobarg9@miumg.edu.gt>
2021-05-15 13:21:06 -0600

    correccion de nombres

--- forms/usuarios/modificacion.php
+++ forms/usuarios/modificacion.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Modificacion</title>
-    <link rel="stylesheet" href="css/New.css">
+    <link rel="stylesheet" href="css/new.css">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 
 
